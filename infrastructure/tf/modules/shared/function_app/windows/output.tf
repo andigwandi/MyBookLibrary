66eb612e3335521c6fbc0179f065829aa5e05b4c
Sanjeev Kumar <andigwandi@gmail.com>
2023-06-02 09:36:06 +0530

    create kvt secret for db

--- infrastructure/tf/modules/shared/function_app/windows/output.tf
+++ infrastructure/tf/modules/shared/function_app/windows/output.tf
@@ -2,7 +2,6 @@ output "function_config" {
   value = {
     default_key           = data.azurerm_function_app_host_keys.key.default_function_key
     id                    = azurerm_windows_function_app.function.id
-    master_key            = data.azurerm_function_app_host_keys.key.primary_key
     name                  = azurerm_windows_function_app.function.name
     outbound_ip_addresses = azurerm_windows_function_app.function.outbound_ip_addresses
     short_name            = local.config.app_name
