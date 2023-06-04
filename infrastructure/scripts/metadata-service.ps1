b3b5bae8fd6883d85a5d859b89da921fc6a0f3d8
Sanjeev Kumar <andigwandi@gmail.com>
2023-06-04 14:23:33 +0530

    meta service script

--- infrastructure/scripts/metadata-service.ps1
+++ infrastructure/scripts/metadata-service.ps1
@@ -9,15 +9,23 @@ function Get-AzureMetadata {
     )
 
     $respraw = Invoke-WebRequest -Headers @{"Metadata" = "true" } -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
-    
-    if ([string]::IsNullOrEmpty($key)) {
-        $metadata = $respraw.Content | ConvertFrom-Json | ConvertTo-Json -Depth 6
-    }
-    else {
-        $metadata = $respraw.Content | Select-Object -ExpandProperty $key |  ConvertFrom-Json | ConvertTo-Json -Depth 6 
-    }
-    
+    $metadata = $respraw.Content
+
     return $metadata
+}
+
+function Get-KeyValues($json, $key) {
+    $obj = $json | ConvertFrom-Json
+    if ($obj.PSObject.Properties.Name -contains $key) {
+        $keyVal = $obj.$key
+        return $keyVal
+    } else {
+        Write-Host "Key '$key' not found in the JSON."
+        return $null  # or return any default value as needed
+    }
+
+
+    return $keyVal
 }
 
 # Main script
@@ -30,6 +38,6 @@ $metadata | Format-List
 
 # Fetch Azure Metadata for a specific key
 if (-not [string]::IsNullOrEmpty($key)) {
-    $value = Get-AzureMetadata -key $key
+    $value = Get-KeyValues -json $metadata -key $key
     Write-Host "Value for key '$key': $value"
 }
